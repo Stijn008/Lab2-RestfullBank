f68f0c8d2581f52eb4812f9cae8df87cbdfc0a19
Stijn Van Raemdonck <van.raemdonck.stijn2@gmail.com>
2023-03-20 09:50:29 +0100

    Remote Client branch change

--- src/main/java/slim_shady/REST/Lab2RestApplication.java
+++ src/main/java/slim_shady/REST/Lab2RestApplication.java
@@ -8,6 +8,20 @@ import slim_shady.REST.Client2;
 @SpringBootApplication
 public class Lab2RestApplication {
 	public static void main(String[] args) throws InterruptedException {
-		SpringApplication.run(Lab2RestApplication.class, args);
+		// Instantiate Client thread
+		Client client = new Client("Client 1");
+		Thread clientThread = new Thread(client);
+
+		// Run Server and Client
+		sleep(5000); 		// Wait 5 seconds (until the server is up and running)
+		clientThread.start();	// Start client thread
+	}
+
+	public static void sleep(int time) {
+		try {
+			Thread.sleep(time); // Wait 5 seconds for server to start up
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
